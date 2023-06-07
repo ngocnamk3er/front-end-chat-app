d5880979ae8e454e0b949b4ccb39e1355f6c5ae3
ngocnam <ngocnamk3er@gmail.com>
2023-06-08 01:49:01 +0700

    add https://chat-app-back-end-ngocnamk3er-gmailcom.vercel.app

--- src/components/SingleChat.js
+++ src/components/SingleChat.js
@@ -16,7 +16,7 @@ import configs from '../config/serverURL'
 import io from "socket.io-client";
 import UpdateGroupChatModal from "./miscellaneous/UpdateGroupChatModal";
 import { ChatState } from "../Context/ChatProvider";
-const ENDPOINT = "http://localhost:5000"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
+const ENDPOINT = "https://chat-app-back-end-ngocnamk3er-gmailcom.vercel.app"; // "https://talk-a-tive.herokuapp.com"; -> After deployment
 var socket, selectedChatCompare;
 
 const SingleChat = ({ fetchAgain, setFetchAgain }) => {
